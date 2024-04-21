d00e20e91d6c3389b278dd4255eff6faf14d01b5
coller <coller@88.com>
2024-04-21 16:55:53 +0800

    add gps

---
+++ gpsx/gps.go
@@ -0,0 +1,29 @@
+package gpsx
+
+import "math"
+
+// 返回单位为：千米
+func DistanceKM(lng1, lat1, lng2, lat2 float64) float64 {
+	radius := 6371000.0 //6378137.0
+	rad := math.Pi / 180.0
+	lat1 = lat1 * rad
+	lng1 = lng1 * rad
+	lat2 = lat2 * rad
+	lng2 = lng2 * rad
+	theta := lng2 - lng1
+	dist := math.Acos(math.Sin(lat1)*math.Sin(lat2) + math.Cos(lat1)*math.Cos(lat2)*math.Cos(theta))
+	return dist * radius / 1000
+}
+
+// 返回米
+func DistanceM(lng1, lat1, lng2, lat2 float64) int {
+	radius := 6371000.0 // 6378137.0
+	rad := math.Pi / 180.0
+	lat1 = lat1 * rad
+	lng1 = lng1 * rad
+	lat2 = lat2 * rad
+	lng2 = lng2 * rad
+	theta := lng2 - lng1
+	dist := math.Acos(math.Sin(lat1)*math.Sin(lat2) + math.Cos(lat1)*math.Cos(lat2)*math.Cos(theta))
+	return int(dist * radius)
+}
